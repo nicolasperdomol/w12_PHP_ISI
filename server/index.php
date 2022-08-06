04475ab6d331a22e8b7699ef33274e9134846f85
nicolas perdomo <nicolasperdomol@hotmail.com>
2022-08-05 20:07:38 -0400

    server_http_request_implementation

--- server/index.php
+++ server/index.php
@@ -35,6 +35,20 @@ switch ($op) {
         webpage::render($pageData);
         break;
 
+    case 50:
+        //file download from server to client
+        // set the file type, here a PDF file, see link below for other file types
+        header('Content-type: application/pdf');
+        // file name is some_file.pdf, the web browser asks to confirm the download
+        header('Content-Disposition: attachment; filename=some_file.pdf');
+        // send out the file, read and send directly with readfile() function
+        readfile('some_file.pdf');
+        break;
+
+    case 51:
+        header("location: http://www.amazon.ca");
+        break;
+
     case 100:
         products::productsList();
         break;
@@ -42,7 +56,7 @@ switch ($op) {
         products::productsCatalogue();
         break;
     default:
-        http_response_code(400);
+        header("HTTP/1.0 400 This operation won't be implemented in the near future, try a different operation.");
         $pageData = DEFAULT_PAGE_DATA;
         $pageData['title'] = "INVALID OPERATION";
         $pageData['description'] = "INVALID OPERATION";
--- server/view/nav.php
+++ server/view/nav.php
@@ -3,4 +3,7 @@
     <a href='index.php?op=0'>Home</a>
     <a href='index.php?op=100'>Product List</a>
     <a href='index.php?op=101'>Product Catalogue</a>
+    <a href='index.php?op=50'>Download</a>
+    <a href='index.php?op=51'>Redirect</a>
+
 </nav>
